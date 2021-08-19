39f7b2cfb796d9f633fd06416623fc81a8f75b69
riley <rileyhawk249@gmail.com>
2021-08-19 19:16:44 +0200

    Added some options for Terminal Power users

--- quickSetup/quickSetup.sh
+++ quickSetup/quickSetup.sh
@@ -1,27 +1,53 @@
 #!/bin/bash
+# This file pth is only for debugging in VSCode
+# source "quickSetup/scripts/codingTools.sh" 
+# source "quickSetup/scripts/graphicsSuite.sh" 
+# source "quickSetup/scripts/multiInstaller.sh"
+# source "quickSetup/scripts/generalTools.sh"
+# source "quickSetup/scripts/colors.sh"
+
 
 source "scripts/codingTools.sh" 
 source "scripts/graphicsSuite.sh" 
 source "scripts/multiInstaller.sh"
 source "scripts/generalTools.sh"
+source "scripts/misc.sh"
 
-echo "#############################################"
-echo "# Welcome to quickstart Script installer 😉 #" 
-echo "# Do you wish to continue yes ✅ or no ❌ ###"
-echo "#############################################"
+function helpMsg() {
+  echo "
+  Shortcuts or flags to trigger bypassing wizard
+  Or install a specific program
+  Command                                   --Options/Long Notation 
+  -c | --coding                             --coding, flag installs tools only in the coding section
+  -g | --graphics                           --graphics, flag installs tools only in the graphics section
+  -m | --minimal                            --minimal, flag installs tools only in the general section
+                                            --help:  prints help message and exits
+    "
+  exit
+}
 
-
-read -r -p "Y/N: " answer
-
-case $answer in 
-    [Yy]* )
-        echo "Beginning Installation"
-	    menuDisplay
+    while [[ "$1" =~ ^- && ! "$1" == "--" ]]; do case $1 in
+      -c | --coding )
+        codingToolsOnly 
+        exit
         ;;
-    [Nn]* )
-        cancelInstall
+      -g | --graphics )
+        graphicsSuite 
+        exit
         ;;
-    *) 
-        echo "Undefined Command"
+      -m | --minimal )
+        generalTools 
+        exit
         ;;
-esac
+      -h | --help )
+        helpMsg
+        ;;
+      * )
+        helpMsg
+        ;;
+    esac; shift; done
+    if [[ "$1" == '--' ]]; then shift; 
+    else 
+      optionsList
+    fi
+
