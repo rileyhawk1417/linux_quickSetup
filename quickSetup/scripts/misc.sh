c3b4577bdb3b6856b53cd951fd98a3e1f5d49269
riley <rileyhawk249@gmail.com>
2022-01-02 16:05:00 +0200

    Added another readMe Function

--- quickSetup/scripts/misc.sh
+++ quickSetup/scripts/misc.sh
@@ -11,7 +11,7 @@ ColourGreen(){
 ColourBlue(){
     echo -ne $blue$1$clear
 }
-: '
+
 ColourGreen(){
     echo -ne $green$1$clear
 }
@@ -19,7 +19,7 @@ ColourGreen(){
 ColourGreen(){
     echo -ne $green$1$clear
 }
-'
+
 # Cheats to avoid repetition
 
 pound='#####'
@@ -36,12 +36,9 @@ install_complete="Installation Completed without errors"
 
 
 function poundPrint(){
-
         for ((y=1 ; y<=11 ; y++ )); do
         echo -ne $pound
         done
-
-
 }
 
 # ReadMe functions to clear other files.
@@ -55,56 +52,104 @@ function CodeReadME(){
     Visual Studio Code - Powerful text editor and IDE, 
     Node - Powerful library for JavaScript and other node related projects,
     Databases - These include Postgresql and Sqlite3
-    JRE -  This is the Java Runtime Environment
     ZSH - Powerful terminal emulator with alot of tricks
     Upload & Download Tools - These include git, wget, curl and other essentials
-    KVM - This is a library for building virtual machines
+    KVM - This is a library for running virtual machines
+    SDKMan - This tool installs SDK's with ease.
     Preset profiles that would make setting up the environment easier
     $nextLine
     Install the tools Y/N: 
     "
 }
 
+function codeTools(){
+    clear
+
+    echo "
+            The following  will be installed :
+            $nextLine
+            KVM - This is a library for running virtual machines
+            SDKMan - This tool installs SDK's with ease. 
+            JDK - Java Development Kit
+            Do you want to install ?
+            $nextLine
+            Y/N
+            "
+}
+
 function generalToolsReadme() {
     clear
 
-    echo "The following will be installed ⚙️ : "
-    echo $nextLine
-    echo "Google-Chrome - Web Browser,"
-    echo "FDM - Download Manager alternative to IDM,"
-    echo "VLC - Powerful Open Source Video Player"
-    echo "Sayonara - Open Source Lightweight music player"
-    echo $nextLine
+    echo "
+            The following will be installed ⚙️ :
+            $nextLine
+            Google-Chrome - Web Browser,
+            FDM - Download Manager alternative to IDM,
+            VLC - Powerful Open Source Video Player
+            Sayonara - Open Source Lightweight music player
+            $nextLine
+            Do you want to install ?
+            $nextLine
+            Y/N
+            "
 }
 
 function cliToolsReadme() {
     clear
     
-    echo "The following Command Line Tools will be installed:"
-    echo $nextLine
-    echo "CMUS - Command line music player with Vim like keybindings"
-    echo "HTOP - Command line system monitor  or task manager"
-    echo "NCDU - Command line disk usage tool"
-    echo "Python - Installs python libraries along with python3 pip"
+    echo "
+            The following Command Line Tools will be installed :
+            $nextLine
+            CMUS - Command line music player with Vim like keybindings
+            HTOP - Command line system monitor or task manager
+            BASHTOP - Command line system monitor 
+            NCDU - Command line disk usage tool
+            Python-Tools - Installs python libraries along with python3 pip
+            Upload & Download Tools - These include git, wget, curl and other essentials
+            $nextLine
+            Continue: Y/N
+            "
 }
 
 function gamesReadme() {
     clear
-    echo "The following gaming tools will be installed:"
-    echo "Steam"
-    echo "Lutris"
+    echo "
+            The following gaming tools will be installed:
+            Steam
+            Lutris
+            Do you want to install ?
+            $nextLine
+            Y/N
+            "
 }
 
 function graphicsReadme() {
     clear
     
-    echo "The following will be installed"
-    echo ""
-    echo "GIMP - Image Manipulator," 
-    echo "Inkscape - Image Manipulator,"
-    echo "OBS - Screencast and Video Editor"
-    echo $nextLine
-    echo "Do you want to install ?"
-    echo "Y/N"
+    echo "
+            The following will be installed
+            $nextLine
+            GIMP - Image Manipulator, 
+            Inkscape - Image Manipulator,
+            OBS - Screencast and Video Editor
+            FlameShot - Screenshot Tool
+            $nextLine
+            Do you want to install ?
+            $nextLine
+            Y/N
+            "
+}
+
+function sdkReadMe(){
+    clear
+
+    echo "
+            Do you want to install Software Development Kits
+            The tools are needed when writing programs and code.
+            $nextLine
+            Do you want to install ?
+            $nextLine
+            Y/N
+            "
 }
 
