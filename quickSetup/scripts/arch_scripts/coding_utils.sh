324bde83256cede6e2f863395b64cd0beba8b74d
Sean Riley Hawkins <rileyhawk249@gmail.com>
2022-01-26 22:00:21 +0200

    Created arch_scripts for Arch Distros

---
+++ quickSetup/scripts/arch_scripts/coding_utils.sh
@@ -0,0 +1,16 @@
+function terminals(){
+    sudo pacman -S kitty guake
+}
+
+function VM_Utils(){
+    # Install necessary libraries
+    sudo pacman -S qemu libvirt iptables-nft dnsmasq virt-manager
+    # Start the service
+    sudo systemctl start libvirtd
+    #Enable the service
+    sudo systemctl enable libvirtd
+    #Allow current user to use service
+    sudo usermod -aG libvirt $USER
+}
+
+function 
---
+++ quickSetup/scripts/arch_scripts/essentials.sh
@@ -0,0 +1,8 @@
+function archivers(){
+    sudo pacman -S p7zip p7zip-plugins unrar tar rsync
+}
+
+function codecs(){
+    sudo pacman -S a52dec faac faad2 flac jasper lame libdca libdv libmad libmpeg2 libtheora libvorbis libxv wavpack x264 xvidcore gstreamer0.10-plugins
+}
+
