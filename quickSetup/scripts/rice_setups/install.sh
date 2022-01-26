9e10161372d978fc7e340a3a175d8a4d9612916c
Sean Riley Hawkins <rileyhawk249@gmail.com>
2022-01-26 21:58:50 +0200

    Created Rice_Setup for awesomeWM

---
+++ quickSetup/scripts/rice_setups/install.sh
@@ -0,0 +1,16 @@
+# Install necessary configs for awesome desktop
+
+function awesomeWM(){
+    sudo pacman -S awesome
+}
+
+function notifications(){
+    sudo pacman -S mpd polybar dunst
+}
+
+function launchers(){
+    sudo pacman -S rofi
+}
+
+
+
