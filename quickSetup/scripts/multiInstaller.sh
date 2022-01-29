258317765edca093a2e017d3bd33da70f6d61b9c
Sean Riley Hawkins <rileyhawk249@gmail.com>
2022-01-29 21:40:35 +0200

    Sourced other scripts

---
+++ quickSetup/scripts/multiInstaller.sh
@@ -0,0 +1,18 @@
+# This script is to automate install as much as possible
+# TODO: Connect the other scripts
+
+source "scripts/codingUtils.sh"
+source "scripts/essentials.sh"
+source "scripts/mediaUtils.sh"
+
+function codingTools(){
+  #Collect functions from there.
+}
+
+function essentials(){
+  #Collect functions from there. 
+}
+
+function mediaUtils(){  
+  #Collect functions from there.
+}
