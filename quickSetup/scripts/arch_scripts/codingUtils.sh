5935418e6cc5b96f323dbe4185968700f5553ac5
Sean Riley Hawkins <rileyhawk249@gmail.com>
2022-01-30 16:50:27 +0200

    Added Github and HomeBrew

--- quickSetup/scripts/arch_scripts/codingUtils.sh
+++ quickSetup/scripts/arch_scripts/codingUtils.sh
@@ -2,6 +2,16 @@ function terminals(){
     sudo pacman -S kitty guake
 }
 
+function homeBrew(){
+  #Grab brew first then install other components
+
+    /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
+}
+
+function githubCLI(){
+    brew install gh
+}
+
 function VM_Utils(){
     # Install necessary libraries
     sudo pacman -S qemu libvirt iptables-nft dnsmasq virt-manager
