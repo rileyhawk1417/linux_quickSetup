d3c02e93e82b54f42073693c1b52bd41ecab94c2
Sean Riley Hawkins <rileyhawk249@gmail.com>
2022-02-07 14:23:42 +0200

    Added noconfirm for pacman and updated lightdm

--- quickSetup/scripts/rice_setups/install.sh
+++ quickSetup/scripts/rice_setups/install.sh
@@ -3,23 +3,35 @@
 # Picom is to reduce screen tearing
 
 function awesomeWM(){
-    sudo pacman -S awesome picom
+    sudo pacman -S awesome picom --noconfirm
 }
 
 function notifications(){
-    sudo pacman -S mpd polybar dunst
+    sudo pacman -S mpd polybar dunst --noconfirm
 }
 
 function launchers(){
-    sudo pacman -S rofi
+    sudo pacman -S rofi --noconfirm
 }
 
 function startUp(){
-    sudo pacman -S plymouth
+    sudo pacman -S plymouth --noconfirm
+}
+
+function lightDM(){
+  
+    # lightdm-plymouth is meant to work with plymouth
+    sudo pacman -S lightdm-plymouth lightdm-webkit2-greeter --noconfirm
+
+    echo "[Seat:*] \n\ngreeter-session=lightdm-webkit2-greeter" >> /etc/lightdm/lightdm.conf
+
+    systemctl enable lightdm-plymouth.service
 }
 
 function displayManagers(){
-    sudo pacman -S lightdm-plymouth
+   lightDM 
 }
+
+
 
 
